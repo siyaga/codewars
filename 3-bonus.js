aacf875c5f8bb98d222c67d82627b3b6c4a3895b
lifeforme <siyaga123@gmail.com>
2024-09-26 22:43:54 +0700

    finish test 3 bonus salary

---
+++ 3-bonus.js
@@ -0,0 +1,10 @@
+function bonusTime(salary, bonus) {
+  // your code here
+  if (bonus == true) {
+    return "£" + salary * 10;
+  } else {
+    return "£" + salary;
+  }
+}
+
+console.log(bonusTime(10000, true));
